d0fbbbe9afdce4cdcbd499bc7fede6ca2ddfc19b
Christopher Mason <cymason@ucsc.edu>
2023-04-19 23:14:46 -0700

    added index and updated main code

--- web-server/main.py
+++ web-server/main.py
@@ -1,32 +1,43 @@
-from flask import Flask, render_template, request
+import os
 import csv
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    # if request.method == 'POST':
-    #     if 'csv_file' not in request.files:
-    #         return "No file uploaded", 400
+# Route to display the HTML page that shows the CSV data
+@app.route('/')
+def display():
+    return render_template('display.html')
 
-    #     file = request.files['csv_file']
-    #     if not file:
-    #         return "No file uploaded", 400
+# Route to return the CSV data in HTML format, without the surrounding HTML tags
+@app.route('/csv_data')
+def csv_data():
+    # Get all CSV files in the assets directory
+    csv_files = [f for f in os.listdir('assets') if f.endswith('.csv')]
 
-    #     if file.filename == '':
-    #         return "No file selected", 400
+    # Initialize a list to hold all the rows from all CSV files
+    rows = []
 
-    #     if file:
-    #         data = []
-    #         stream = io.StringIO(file.stream.read().decode("UTF8"), newline=None)
-    #         csv_input = csv.reader(stream)
-    #         for row in csv_input:
-    #             data.append(row)
-    #         return render_template('display.html', data=data)
-    # return render_template('index.html')
-    return "Hello World Flask"
+    # Loop through each CSV file
+    for csv_file in csv_files:
+        # Open the CSV file and read its contents
+        with open(os.path.join('assets', csv_file)) as f:
+            csv_reader = csv.reader(f)
+            # Skip the header row
+            next(csv_reader)
+            # Add each row to the list of rows
+            rows.extend(csv_reader)
+
+    # Get the header row from the first CSV file
+    with open(os.path.join('assets', csv_files[0])) as f:
+        csv_reader = csv.reader(f)
+        headers = next(csv_reader)
+
+    # Render just the table data as HTML, without the surrounding HTML tags
+    table_html = render_template('table_data.html', headers=headers, rows=rows)
+
+    return jsonify({'table_html': table_html})
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
-    # app.run(debug=True)
 
